8b3a6ee8935a502a8ace5bdb47ceb0b4df5ca124
Ningyuan <pny1989@gmail.com>
2016-09-18 22:00:05 +1000

    add header

---
+++ client.c
@@ -0,0 +1,10 @@
+#include <stdio.h>      
+#include <stdlib.h> 
+#include <string.h>
+#include <stddef.h> 
+#include <unistd.h>    
+#include <time.h>
+#include <pthread.h> 
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/socket.h>
