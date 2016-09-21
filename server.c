6c1c6eece5bf48e86551ea1a01816a2c21edec1b
Ningyuan <pny1989@gmail.com>
2016-09-21 15:03:22 +1000

    successfully retrive data from authentication.txt

--- server.c
+++ server.c
@@ -14,40 +14,61 @@
 #include <errno.h> 
 #include <arpa/inet.h>
 
+
+#define AUTH_LINE_NUM 10
+#define LINE_BUF_SIZE 50
+#define DATA_BUF_SIZE 10
+
 typedef struct login{
 	char* username;
 	int pin;
 	int client_no;
 } login_t;
 
-
-
-int main(int argc, char *argv[]){
-	FILE* fp = fopen("./data/Authentication.txt","r");
-	char* buffer = (char* )malloc(100 * sizeof(char));
-	login_t user_info;
-	user_info.username = malloc(20 * sizeof(char));
-	//user_info.pin = malloc(20 * sizeof(char));
-	//user_info.client_no = malloc(20 * sizeof(char));
-	
-	if(!fp){
+void get_authentication(login_t auth_info[]){
+	FILE* file = fopen("./data/Authentication.txt","r");	
+	if(!file){
 		printf("can't open file\n");
-	
 	}
-	printf("hello world");
-	fgets(buffer,100 * sizeof(char),fp);
-	//printf("%s",buffer);
-	sscanf(buffer,"%s",user_info.username);
-	sscanf(buffer,"%*s%d",user_info.pin);
-	sscanf(buffer,"%*s%*d%d", user_info.client_no);
-	printf("\n%s   %d     %d", user_info.username, user_info.pin, user_info.client_no);
+	char* line[AUTH_LINE_NUM + 1];
+	char* pin_buf = malloc(DATA_BUF_SIZE * sizeof(char));
+	char* client_buf = malloc(DATA_BUF_SIZE * sizeof(char));
+	int j = 0;
+	for(int i = 0; i < AUTH_LINE_NUM + 1; i++){
+		line[i] = (char*)malloc(LINE_BUF_SIZE * sizeof(char));
+		fgets(line[i], LINE_BUF_SIZE * sizeof(char), file);            
+		if(i > 0){
+			auth_info[j].username = malloc(DATA_BUF_SIZE * sizeof(char));	
+			sscanf(line[i], "%s", auth_info[j].username);
+			sscanf(line[i], "%*s%s", pin_buf);
+			auth_info[j].pin = atoi(pin_buf);			
+			sscanf(line[i], "%*s%*s%s", client_buf);
+			auth_info[j].client_no = atoi(client_buf);
+			j++;
+		}
+		free(line[i]);
+		line[i] = NULL;
+	}
+	free(pin_buf);
+	pin_buf = NULL;
+	free(client_buf);
+	client_buf = NULL;
+	fclose(file);
+}
 
 
-	
-	
-	fclose(fp);
-	free(buffer);
-	buffer = NULL;
 
+
+int main(int argc, char *argv[]){	
+	login_t auth_info[AUTH_LINE_NUM];
+	for(int i = 0; i < AUTH_LINE_NUM ; i++){
+		auth_info[i].username = "C";
+		auth_info[i].pin = 1;
+		auth_info[i].client_no = 1;
+	}
+	get_authentication(auth_info);
+	for(int i = 0; i < AUTH_LINE_NUM ; i++){
+		printf("%s        %d         %d\n", auth_info[i].username, auth_info[i].pin, auth_info[i].client_no);
+	}
 	return 0;
 }
